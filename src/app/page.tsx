1f6e03668ce29fc70b3545fd9b2a31e73491f9c9
Yusuf Kaplan <yusufdenizkaplan@gmail.com>
2023-11-12 03:05:09 +0000

    added csv, added background colour, added radio icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import RadioWheel from "./components/RadioWheel";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center ">
-      {/* <div className="">GTA V RADIO</div> */}
-      <div className="flex flex-col items-center justify-center">
+    <main className=" flex min-h-screen flex-col items-center justify-center bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-fuchsia-300 via-green-400 to-rose-700 ">
+      <div className="flex flex-col items-center justify-center ">
         <RadioWheel />
       </div>
     </main>
